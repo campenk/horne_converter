9022e3dce6df9a77821489bbbdf5467e187501ff
campenk <88216821+campenk@users.noreply.github.com>
2023-05-26 13:10:14 +1200

    tailwind working

--- app/app/views.py
+++ app/app/views.py
@@ -1,13 +1,24 @@
 from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 from .models import Item, MeasurementType
 
 
-def converter(request, entry, unit, output_category):
-    unit_category = MeasurementType.objects.filter()
-    a_list = Item.objects.filter()
-    return render(request, 'news/year_archive.html', a_list)
+# def converter(request, entry, unit, output_category):
+#     unit_category = MeasurementType.objects.filter()
+#     a_list = Item.objects.filter()
+#     return render(request, 'news/year_archive.html', a_list)
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = "Q1", "Q2", "Q3"
+    template = loader.get_template('app/home.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def home(request):
+    return HttpResponse("You are at the home view")
+
