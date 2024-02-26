081c05cbf4904c87f9b14d8f6e3c51622ee71a3b
campenk <88216821+campenk@users.noreply.github.com>
2024-02-27 09:28:15 +1300

    fix text errors

--- app/app/form.py
+++ app/app/form.py
@@ -7,7 +7,7 @@ class UserEntryForm(forms.Form):
 
     input_value = forms.DecimalField()
     input_unit = forms.ChoiceField(choices=UNIT_CHOICES, label="unit")
-    output_category = forms.ModelChoiceField(ItemCategory.objects.all(), empty_label="random", required=False)
+    output_category = forms.ModelChoiceField(ItemCategory.objects.all(), empty_label="Random", required=False)
 
 
 
